446deb1330ab22f7fd06168095db961359d23dfe
shani barak <shanibrk@gmail.com>
2022-06-05 11:55:28 +0300

    adding crud to post.still got an issue with google that need to be fixed

--- fronted/src/components/Pin.jsx
+++ fronted/src/components/Pin.jsx
@@ -5,11 +5,40 @@ import { client,urlFor } from '../client'
 import {MdDownloadForOffline}from 'react-icons/md'
 import {AiTwotoneDelete} from 'react-icons/ai';
 import {BsFillArrowUpRightCircleFill} from 'react-icons/bs'
+import{fetchUser} from '../utils/fetchUser'
 
-const Pin = ({pin:{postegBy, image, _id, destination}}) => {
+const Pin = ({pin:{postedBy, image, _id, destination, save}}) => {
     const [postHoverd,setPostHoverd]=useState(false)
     const[savingPost, setSavingPost]=useState(false)
     const navigate=useNavigate()
+    const user= localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
+    const alreadySaved= !!(save?.filter((item)=> item.postedBy._id===user.googleId))?.length
+    const savePin=(id)=>{
+        if(!alreadySaved){
+            setSavingPost(true);
+            client.patch(id).setIfMissing({save:[]})
+            .insert('after', 'save[-1', [{
+                _key:uuid(),
+                userId:user.googleId,
+                postedBy:{
+                    _type:'posteddBy',
+                    _ref:user.googleId
+                }
+            }])
+            .commit()
+            .then(()=>{
+                window.location.reload()
+                setSavingPost(false)
+            })
+        }
+    }
+    const deletePin=(id)=>{
+        client.delete(id)
+        .then(()=>{
+            window.location.reload()
+        })
+
+    }
   return (
     <div className='m-2'>
         <div onMouseEnter={()=>setPostHoverd(true)}
@@ -31,10 +60,43 @@ const Pin = ({pin:{postegBy, image, _id, destination}}) => {
                          </a>
 
                      </div>
+         {alreadySaved? (
+             <button type='button' className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 rounded-3xl hover:shadow-md outlined-none'>{save?.length} Saved</button>
+            ):
+            (<button onClick={(e)=> {
+                e.stopPropagation()
+                savePin(_id)
+            }} type='button' className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 rounded-3xl hover:shadow-md outlined-none'>save</button>)}
+
+                </div>
+                <div className='flex justify-between items-center gap-2 w-full '>
+                    {destination && (
+                        <a 
+                        href={destination}
+                        target='_blank'
+                        rel="noreferrer"
+                        className='bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:100'
+                        >
+                            <BsFillArrowUpRightCircleFill/>
+                            {destination.slice(8)}
+                        </a>
+                    )}
+                    {postedBy?._id=== user.googleId &&(
+                        <button type='button' onClick={(e)=>{
+                            e.stopPropagation();
+                            deletePin(_id)
+                        }} className='bg-white opacity-70 hover:opacity-100 font-bold rounded-3xl text-dark hover:shadow-md outlined-none'>
+                            <AiTwotoneDelete/>
+                        </button>
+                    )}
                 </div>
              </div>
          )}
         </div>
+        <Link to={`user-profile/${postedBy?._id}`} className='flex gap-2 nt-2 items-center'>
+            <img className='w-8 h-8 rounded-full object-cover' src={postedBy?.image} alt="user-profile" />
+            <p className='font-semibold ca[italize'>{postedBy?.userName}</p>
+        </Link>
         
     </div>
   )
---
+++ fronted/src/utils/fetchUser.js
@@ -0,0 +1,4 @@
+const fetchUser=()=>{
+    const userInfo= localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
+    return userInfo
+}
